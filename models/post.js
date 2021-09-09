c84a44ea55413f3fef8394800278bcabdf4319fc
Yitik Kawatra <yitikkawatra@gmail.com>
2021-09-09 15:27:45 +0530

    post functions

--- models/post.js
+++ models/post.js
@@ -1,4 +1,6 @@
 const mongoose=require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
 
 const postSchema= new mongoose.Schema({
     title:{
@@ -9,6 +11,18 @@ const postSchema= new mongoose.Schema({
     body:{
         type:String,
         required:true,
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    postedBy: {
+        type: ObjectId,
+        ref: 'User'
+    },
+    created: {
+        type: Date,
+        default: Date.now
     }
 })
 
--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,21 @@
 const express=require('express');
 
-const {getPosts,createPost}=require('../controllers/post');
+const {getPosts,createPost,postsByUser,postById,isPoster,deletePost,updatePost}=require('../controllers/post');
 const {userById}=require('../controllers/user');
 const {createPostValidator}=require('../validator/index');
 const {requireSignin}=require('../controllers/auth');
 const router=express.Router();
 
-router.get('/',getPosts);
+router.get('/posts',getPosts);
 
-router.post('/post',requireSignin,createPostValidator,createPost);
+router.post('/post/new/:userId',requireSignin,createPost,createPostValidator);
+
+router.get('/posts/by/:userId',requireSignin,postsByUser)
+router.delete('/post/:postId', requireSignin, isPoster, deletePost);
+router.put('/post/:postId', requireSignin, isPoster, updatePost);
+
 router.param("userId",userById);
+router.param("postId",postById);
+
+
 module.exports=router;
